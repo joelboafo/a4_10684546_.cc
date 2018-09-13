bff42d1e9e0304ac90f298c7260476fb267984c5
joelboafo <42918153+joelboafo@users.noreply.github.com>
2018-09-13 02:14:55 +0100

    Add files via upload

---
+++ a4_10684546.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+  int gcd (int a, int b)
+{
+ int ans;
+ ans=b%a;
+ if (ans==0)
+    {
+        return b; 
+    }
+  else
+   {
+    a=b;
+    b=ans;
+    gcd(b,a);
+   }
+}
+int main()
+{
+cout<<"Calculation of the GCF of two numbers"<<endl;
+int large;
+int small;
+int result;
+cout<<"enter the large number"<<endl;
+cin>>large;
+cout<<"enter the small number"<<endl;
+cin>>small;
+result=gcd(large,small);
+cout<<result<<endl;
+
+system ("PAUSE");
+       return 0;
+}
